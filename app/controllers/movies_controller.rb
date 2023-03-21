a84b7927d40dfc9f7e565a2be7f0cda714fafa91
GladysKigen <gladys.kigen@student.moringa.com>
2023-03-21 17:05:49 +0300

    created the CRUD actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,37 @@
 class MoviesController < ApplicationController
-  
+  wrap_parameters format: []
+
+  #GET /movies
   def index
     movies = Movie.all
     render json: movies
+  end
+
+  #POST /movies/:id
+  def create
+     movie = Movie.create(movie_params)
+     render json: movie, status: :created 
+  end
+
+  #PATCH /movies/:id
+  def update
+     movie = Movie.find(params[:id])
+     movie.update(movie_params)
+     render json: movie
+  end
+
+  # DELETE /movies/:id
+  def destroy
+      movie = Movie.find(params[:id])
+      movie.destroy
+      head :no_content
+  end
+
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
 
 end
